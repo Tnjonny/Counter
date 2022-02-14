c79746c2b40cb361809dd995239897d24e5a78db
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-02-14 22:52:37 +0000

    commit

--- counter.js
+++ counter.js
@@ -3,3 +3,8 @@ const count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+    })
+});
